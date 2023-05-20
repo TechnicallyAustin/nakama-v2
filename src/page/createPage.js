8055785f53536a99f37e91d901b5895c867fc3b4
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-05-20 00:41:40 -0700

    styling page

--- src/page/createPage.js
+++ src/page/createPage.js
@@ -3,15 +3,15 @@ export function createPage(){
     const page = {
         header: function(){
             const header = pageContent.appendChild(document.createElement('header'));
-            header.classList.add('page-header');
+            header.setAttribute('class', 'page-header w-100 h-100 bg-dark');
         },
         main: function(){
             const main = pageContent.appendChild(document.createElement('main'));
-            main.classList.add('page-main');
+            main.setAttribute('class', 'page-main w-100 h-100 bg-info');
         },
         footer: function(){
             const footer = pageContent.appendChild(document.createElement('footer'));
-            footer.classList.add('page-footer');
+            footer.setAttribute('class', 'page-footer w-100 h-100 bg-dark');
         }
     };
     page.header();
