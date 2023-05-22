e6a7995597ed360d631f9ad4045f8e3fd71d383b
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-05-21 19:21:56 -0700

    adding a search bar

--- src/content/createContent.js
+++ src/content/createContent.js
@@ -1,10 +1,11 @@
 import { footerContent } from "./footerContent";
 import { headerContent } from "./headerContent";
+import { heroContent } from "./heroContent";
 import { mainContent } from "./mainContent";
 
 export function createContent(){
     headerContent()
-
     mainContent()
+    heroContent()
     footerContent()
 }
