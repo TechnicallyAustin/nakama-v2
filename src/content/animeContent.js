bb3d2dd6faae0b77a75c510e04dbcbc25a8de237
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-05-21 23:32:46 -0700

    styling anime container

--- src/content/animeContent.js
+++ src/content/animeContent.js
@@ -6,14 +6,14 @@ export function animeContent(){
         let value = title.toLowerCase();
 
         let container = animeContainer.appendChild(document.createElement('div'));
-        container.setAttribute('class', `${value}-container display-container d-flex flex-column justify-content-start align-items-center mx-3`);
+        container.setAttribute('class', `${value}-container display-container d-flex flex-column justify-content-around align-items-start mx-3`);
 
         let label = container.appendChild(document.createElement('h3'));
-        label.setAttribute('class', `${value}-label text-white pt-1`);
+        label.setAttribute('class', `${value}-label text-white pt-3`);
         label.textContent = title;
 
         let list = container.appendChild(document.createElement('div'));
-        list.setAttribute('class', `${value}-anime anime-list bg-light`);
+        list.setAttribute('class', `${value}-anime anime-list bg-light rounded`);
     }
 }
 
